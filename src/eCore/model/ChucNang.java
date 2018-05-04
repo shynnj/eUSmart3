1bce73c7e2f6161edeb3c9ebc8ae79ed79b11729
Nghia <thanhnghia1702@gmail.com>
2018-05-04 09:47:21 +0700

    up bi1

--- src/eCore/model/ChucNang.java
+++ src/eCore/model/ChucNang.java
@@ -110,73 +110,7 @@ public class ChucNang implements Comparable<ChucNang> {
 		this.thoiGianCapNhat = thoiGianCapNhat;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((duongDan == null) ? 0 : duongDan.hashCode());
-		result = prime * result + ((ghiChu == null) ? 0 : ghiChu.hashCode());
-		result = prime * result + ((hinhAnh == null) ? 0 : hinhAnh.hashCode());
-		result = prime * result + ((maChucNang == null) ? 0 : maChucNang.hashCode());
-		result = prime * result + ((ChucNangCha == null) ? 0 : ChucNangCha.hashCode());
-		result = prime * result + ((moTa == null) ? 0 : moTa.hashCode());
-		result = prime * result + ((tenHienThi == null) ? 0 : tenHienThi.hashCode());
-		result = prime * result + ((thoiGianCapNhat == null) ? 0 : thoiGianCapNhat.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		ChucNang other = (ChucNang) obj;
-		if (duongDan == null) {
-			if (other.duongDan != null)
-				return false;
-		} else if (!duongDan.equals(other.duongDan))
-			return false;
-		if (ghiChu == null) {
-			if (other.ghiChu != null)
-				return false;
-		} else if (!ghiChu.equals(other.ghiChu))
-			return false;
-		if (hinhAnh == null) {
-			if (other.hinhAnh != null)
-				return false;
-		} else if (!hinhAnh.equals(other.hinhAnh))
-			return false;
-		if (maChucNang == null) {
-			if (other.maChucNang != null)
-				return false;
-		} else if (!maChucNang.equals(other.maChucNang))
-			return false;
-		if (ChucNangCha == null) {
-			if (other.ChucNangCha != null)
-				return false;
-		} else if (!ChucNangCha.equals(other.ChucNangCha))
-			return false;
-		if (moTa == null) {
-			if (other.moTa != null)
-				return false;
-		} else if (!moTa.equals(other.moTa))
-			return false;
-		if (tenHienThi == null) {
-			if (other.tenHienThi != null)
-				return false;
-		} else if (!tenHienThi.equals(other.tenHienThi))
-			return false;
-		if (thoiGianCapNhat == null) {
-			if (other.thoiGianCapNhat != null)
-				return false;
-		} else if (!thoiGianCapNhat.equals(other.thoiGianCapNhat))
-			return false;
-		return true;
-	}
-
+	
 	@Override
 	public String toString() {
 		return "ChucNang [maChucNang=" + maChucNang + ", tenHienThi=" + tenHienThi + ", duongDan=" + duongDan
